086f03a66af65f9cc1e0ee77dc2cbed9d9d12dfa
farhaanali200 <mohdfarhaanali@gmail.com>
2022-09-09 21:20:49 +0530

    "button and themes"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {Button, styled, Typography} from "@mui/material";
+import {Settings,Add} from '@mui/icons-material';
 
 function App() {
+
+    const BlueButton = styled(Button) (({theme}) =>({
+        backgroundColor: "skyblue",
+        color: "white",
+        margin: 5,
+        "&:hover": {
+            backgroundColor: "red"
+        },
+        "&:disabled": {
+            backgroundColor: "yellow"
+        }
+    }));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Button startIcon={<Settings/>} color="secondary" size="small">Settings</Button>
+        <Button startIcon={<Add/>} variant="contained" color="success" size="small">
+            Add new post
+        </Button>
+        <Typography variant="h1" component="p">
+            It uses h1 style but its a p tag
+        </Typography>;
+        <Button variant="contained"
+        sx ={{
+            backgroundColor: "blue",
+            color: "white",
+            margin: 5,
+            "&:hover": {
+                backgroundColor: "red"
+            },
+            "&:disabled": {
+                backgroundColor: "yellow"
+            }
+        }}
+        >My unique Button</Button>
+        <BlueButton>Another Button</BlueButton>
     </div>
   );
 }
